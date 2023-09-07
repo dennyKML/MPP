9f00271d40ad3dbda1b3e1e150872c0a26dda5cd
dennyKML <denis.kimlyk.of@gmail.com>
2023-09-07 18:38:28 +0300

    Through cmd
    Game args are passed through cmd

--- pr1.rb
+++ pr1.rb
@@ -1,23 +1,14 @@
-variants = %w[Rock Paper Scissors]
+conditions = {"Rock" => "Scissors", "Paper" => "Rock", "Scissors" => "Paper"}
 
-while true
-  puts("\nMake your choice: Rock, Paper or Scissors (exit to leave)")
-  usr_choice = gets.chop.capitalize
+usr_choice = ARGV[0]
+cmp_choice = ARGV[1]
 
-  if usr_choice == "Exit"
-    break
-  end
-
-  cmp_choice = variants[rand(0..2)]
-  puts("Computer choice - #{cmp_choice}")
-
-  if usr_choice == cmp_choice
-    puts("Draw!")
-  elsif (usr_choice == variants[0] and cmp_choice == variants[1]) or (usr_choice == variants[1] && cmp_choice == variants[2]) or (usr_choice == variants[2] && cmp_choice == variants[0])
-    puts("You lost!")
-  elsif (usr_choice == variants[0] and cmp_choice == variants[2]) or (usr_choice == variants[1] && cmp_choice == variants[0]) or (usr_choice == variants[2] && cmp_choice == variants[1])
-    puts("You won!")
-  else
-    puts("I can't speak wrong, sorry!")
-  end
+if conditions[usr_choice] == cmp_choice
+  puts("You won!")
+elsif usr_choice == cmp_choice
+  puts("Draw!")
+elsif conditions[cmp_choice] == usr_choice
+  puts("You lose!")
+else
+  puts("Sorry, I can't speak wrong!")
 end
