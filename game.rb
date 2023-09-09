084ee6c423677d90b56aa5d5f7e3edd3c6c52921
dennyKML <denis.kimlyk.of@gmail.com>
2023-09-09 18:23:53 +0300

    Little update
    Checking for the number of input arguments

--- game.rb
+++ game.rb
@@ -1,14 +1,18 @@
 conditions = {"Rock" => "Scissors", "Paper" => "Rock", "Scissors" => "Paper"}
 
-usr_choice = ARGV[0]
-cmp_choice = ARGV[1]
+if ARGV.length == 2
+  usr_choice = ARGV[0]
+  cmp_choice = ARGV[1]
 
-if conditions[usr_choice] == cmp_choice
-  puts("You won!")
-elsif usr_choice == cmp_choice
-  puts("Draw!")
-elsif conditions[cmp_choice] == usr_choice
-  puts("You lose!")
+  if conditions[usr_choice] == cmp_choice
+    puts("You won!")
+  elsif usr_choice == cmp_choice
+    puts("Draw!")
+  elsif conditions[cmp_choice] == usr_choice
+    puts("You lose!")
+  else
+    puts("Sorry, I can't speak wrong!")
+  end
 else
-  puts("Sorry, I can't speak wrong!")
+  puts("Give me two arguments!")
 end
